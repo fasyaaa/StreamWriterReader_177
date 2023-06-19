06887588edeaf2975d8ba85b6c334a8aec79f9b4
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-06-19 14:49:55 +0700

    Add project files.

---
+++ StreamWriterReader_177/Iofile.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	//membukan file dalam mode menulis
+	ofstream outfile;
+	//menunjukkan ke sebuah nama file
+	outfile.open("contoh file.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q")break;
+
+	}
+}
