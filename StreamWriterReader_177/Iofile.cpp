747d11336d5201b5ccf16391e2c24efffb9e8b47
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-06-19 14:56:54 +0700

    8

--- StreamWriterReader_177/Iofile.cpp
+++ StreamWriterReader_177/Iofile.cpp
@@ -20,6 +20,23 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukkan karakter q
 		if (baris == "q")break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
 
+	//Membuka file dalam mode membaca
+	ifstream infile;
+	//Menunjuk ke sebuah file
+	infile.open("contoh file.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//Jika file ada maka
+	if (infile.is_open()) {
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris)) {
+
+		}
 	}
 }
